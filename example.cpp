f3c48359e69b09d0c75cd3d40284fadcc26b47d3
Nelly Nemysheva <95230345+nelhork@users.noreply.github.com>
2024-08-30 08:53:05 +0400

    Added a function that is handled by threads

--- example.cpp
+++ example.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <cmath>
 
 #include "ThreadPool.h"
 
+void operate(char letter) {
+    double x = 12345.6789;
+    for (int i = 0; i < 10000; i++) {
+        for (int j = 0; j < 10000; j++) {
+            x = sqrt(x);
+            x = x + 0.000000001;
+            x = pow(x, 2);
+        }
+    }
+}
+
 int main()
 {
-    
-    ThreadPool pool(4);
-    std::vector< std::future<int> > results;
+    const int numThreads = 5;
+    ThreadPool pool(numThreads);
+    std::vector<char> letters = {'A', 'B', 'C', 'D', 'E'};
 
-    for(int i = 0; i < 8; ++i) {
-        results.emplace_back(
-            pool.enqueue([i] {
-                std::cout << "hello " << i << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " << i << std::endl;
-                return i*i;
-            })
-        );
+    for(int i = 0; i < letters.size(); ++i)
+    {
+        pool.enqueue([ letter = letters[i] ] {
+            operate(letter);
+        });
     }
-
-    for(auto && result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
     
     return 0;
 }
